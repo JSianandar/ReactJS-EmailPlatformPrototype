d92fe3ca57b03b707bc033dc71dc4938f1b8f1c9
Jason Sianandar <sianandarjason@gmail.com>
2025-01-13 11:02:42 +0700

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Include all files in src folder
+  darkMode: "class", // Enable dark mode using a CSS class
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
